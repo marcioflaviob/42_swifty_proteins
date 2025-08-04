e99a028377813abb00e28a0f63fa0784545c48af
Marcio Flavio <oi@marcioflavio.com>
2025-08-04 20:06:14 +0200

    Return to login page when app is minimize

--- lib/main.dart
+++ lib/main.dart
@@ -4,42 +4,73 @@ import 'providers/protein_provider.dart';
 import 'providers/auth_provider.dart';
 import 'screens/protein_list_screen.dart';
 import 'screens/login_screen.dart';
+import 'utils/auth_navigator_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
-  runApp(const SwiftyProteinsApp());
-}
-
-class SwiftyProteinsApp extends StatelessWidget {
-  const SwiftyProteinsApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiProvider(
+  runApp(
+    MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => AuthProvider()),
         ChangeNotifierProvider(create: (context) => ProteinProvider()),
       ],
-      child: MaterialApp(
-        title: 'Swifty Proteins',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-          useMaterial3: true,
-        ),
-        initialRoute: '/',
-        routes: {
-          '/': (context) => Consumer<AuthProvider>(
-            builder: (context, authProvider, child) {
-              return authProvider.isAuthenticated 
-                  ? const ProteinListScreen() 
-                  : const LoginScreen();
-            },
-          ),
-          '/login': (context) => const LoginScreen(),
-          '/proteins': (context) => const ProteinListScreen(),
-        },
+      child: const SwiftyProteinsApp(),
+    ),
+  );
+}
+
+class SwiftyProteinsApp extends StatefulWidget {
+  const SwiftyProteinsApp({super.key});
+
+  @override
+  State<SwiftyProteinsApp> createState() => _SwiftyProteinsAppState();
+}
+
+class _SwiftyProteinsAppState extends State<SwiftyProteinsApp> with WidgetsBindingObserver {
+  final AuthNavigatorObserver _authNavigatorObserver = AuthNavigatorObserver();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _authNavigatorObserver.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      Provider.of<AuthProvider>(context, listen: false).logout();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Swifty Proteins',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
       ),
+      navigatorObservers: [_authNavigatorObserver],
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Consumer<AuthProvider>(
+          builder: (context, authProvider, child) {
+            return authProvider.isAuthenticated 
+                ? const ProteinListScreen() 
+                : const LoginScreen();
+          },
+        ),
+        '/login': (context) => const LoginScreen(),
+        '/proteins': (context) => const ProteinListScreen(),
+      },
     );
   }
 }
